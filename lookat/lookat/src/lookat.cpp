ba7e8eb13f24972e362653f7d2ace1ded1d6dd38
Sandra Pareja Medina <sandraa_03@hotmail.es>
2017-02-09 19:44:10 +0100

    Add files via upload

---
+++ lookat/lookat/src/lookat.cpp
@@ -0,0 +1,366 @@
+/*
+ * lookat.cpp
+ *
+ *  Created on: 4 ene. 2017
+ *      Author: robert
+ */
+//OpenCV
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "opencv2/objdetect.hpp"
+#include "opencv2/imgproc.hpp"
+
+//std
+#include <iostream>
+#include <cmath>
+
+#include "ros/ros.h"
+#include "ros/package.h"
+#include "geometry_msgs/Point.h"
+
+
+// Max variation constant
+const int TOTAL_VARIATION = 400;
+
+using namespace cv;
+using namespace std;
+
+/** Function Headers */
+Point detectFace(Mat frame, Point lastPoint);
+int distEuclidean(int x1,int x2, int y1, int y2);
+
+/** Global variables */
+//String face_cascade_name = "haarcascade_frontalface_alt.xml";
+CascadeClassifier face_cascade;
+
+int main(int argc, char **argv)
+{
+	VideoCapture camera;
+	Mat frame;
+	int cam_id;
+	Point prevPoint;
+
+	// Variable to indicate the face is found
+	bool found = false;
+
+	// Kalman filter definition
+
+	// Variable to adjust dT
+    //double ticks = 0;
+
+    // Number of frames without face detetion
+    int numFrames = 0;
+
+    //Define the Kalman filter
+    cv::KalmanFilter KF(4,2,0);	// 4 dynamic parameters
+    							// 2 measure parameters
+    							// 0 control variables
+
+    // Variable to store the measures
+    cv::Mat measure(2, 1, CV_32F); // 2 rows x 1 column of float
+
+	ros::init(argc, argv, "point_publisher");
+	ros::NodeHandle n;
+
+
+	ros::Publisher point_pub = n.advertise<geometry_msgs::Point>("pointpub", 1000);
+	ros::Rate loop_rate(25);
+
+	std::string face_cascade_name_std;
+	if (!n.getParam("face_cascade_name", face_cascade_name_std))
+	      ROS_ERROR("Could not get face_cascade_name");
+	cv::String face_cascade_name = face_cascade_name_std;
+
+	//Check args
+	switch(argc)
+	{
+		case 1: //no argument provided, so try /dev/video0
+			cam_id = 1;
+			break;
+		case 2: //an argument is provided. Get it and set cam_id
+			cam_id = atoi(argv[1]);
+			break;
+		default:
+			std::cout << "Invalid number of arguments. Call program as: webcam_capture [video_device_id]. " << std::endl;
+			std::cout << "EXIT program." << std::endl;
+			break;
+	}
+
+
+	cout << "Opening video device " << cam_id << endl;
+
+	// Init de Kalman filter
+    // Initialize the transition matrix as a identity matrix 4 x 4
+    cv::setIdentity(KF.transitionMatrix, cv::Scalar::all(1.0f));
+
+    // Set the identity matrix to meaasurement
+    cv::setIdentity(KF.measurementMatrix);
+
+    // Init the measurement Noise covariance matrix as identity matrix of 1e-1
+    cv::setIdentity(KF.measurementNoiseCov, cv::Scalar::all(1e-1));
+
+    // Init the process noise covariance matrix as identity matrix of 1e-2
+    cv::setIdentity(KF.processNoiseCov, cv::Scalar::all(.005));
+
+    cv::setIdentity(KF.errorCovPost, cv::Scalar::all(.1));
+
+    // Init the measure results
+    measure.setTo(cv::Scalar::all(0.0f));
+
+    ros::Time ticks, preTick;
+
+    // Init time
+    ticks = ros::Time::now();
+    preTick = ticks;
+
+	//open the video stream and make sure it's opened
+    if( !camera.open(cam_id) )
+	{
+        std::cout << "Error opening the camera. May be invalid device id. EXIT program." << std::endl;
+        return -1;
+    }
+
+    // Set the size of the captures
+	camera.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+   	camera.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+
+//	Load the Cascades
+	if( !face_cascade.load( face_cascade_name ) )
+	{ 
+		printf("--(!)Error loading face cascade\n"); 
+		return -1; 
+	}
+
+	while (ros::ok())
+    {
+		geometry_msgs::Point p;
+
+		// Kalman filter center
+		cv::Point kalmanCenter(320,240);
+
+		// dT calculation
+    	//double preTick = ticks;
+    	//ticks = (double) cv::getTickCount();
+		ticks  = ros::Time::now();
+
+        //double dT = (ticks - preTick) / cv::getTickFrequency(); // Seconds
+        double dT = (ticks - preTick).toSec(); // Seconds
+
+		bool bSuccess = camera.read(frame);
+
+	    if (!bSuccess) //if not success, break loop
+	    {
+		   cout << "Cannot read the frame from video file" << endl;
+		   break;
+		}
+		
+		if(found)
+		{
+			// Update the Kalman filter values
+
+	        KF.transitionMatrix.at<float>(3) = dT;
+	        KF.transitionMatrix.at<float>(7) = dT;
+
+	        // Predict the new position
+	        cv::Mat state(4 , 1, CV_32F); // 4 Results, 1 column, float
+
+	        state = KF.predict();
+
+	        kalmanCenter = cv::Point(state.at<float>(0), state.at<float>(1));
+
+	        // Print the center of the image
+			// Print the center of the estimation
+			cv::circle(	frame,			// Destination image
+						kalmanCenter,	// Center of the detection
+						2, 				// Radius
+						CV_RGB(255,0,0),// Color
+						-1);			// Fill the circle
+
+
+			// Print a rectangle 
+			cv::rectangle(	frame, 				// Destination image
+							cv::Point(kalmanCenter.x-10,kalmanCenter.y-10),	// left sup vertex
+							cv::Point(kalmanCenter.x+10,kalmanCenter.y+10), // right inf vertex
+							CV_RGB(255,0,0), 	// Color
+							2);		// Fill the circle
+	    }
+
+	    Point pnt = detectFace(frame, prevPoint);
+
+	    // Verify the detection is correct
+	    if((pnt.x!=1000)&&(pnt.y!=1000))
+	    {
+	    	// There is a detection
+
+	        if(!found)
+        	{
+        		// First detection
+
+        		// Correct the number of Frames
+	        	numFrames = 0;
+
+	        	// Update the measure
+	        	measure.at<float>(0) = pnt.x;
+				measure.at<float>(1) = pnt.y;
+	        	
+	        	// Update the previus point
+	        	prevPoint = pnt;
+
+        		// Init the values of Kalman filter
+				cv::setIdentity(KF.errorCovPre, cv::Scalar::all(0.1f));
+
+				KF.statePost.at<float>(0) = measure.at<float>(0);
+				KF.statePost.at<float>(1) = measure.at<float>(1);
+				KF.statePost.at<float>(2) = 0.0f;
+				KF.statePost.at<float>(3) = 0.0f;
+
+				//cout << "Lo he recuperado!!" << numFrames << std::endl;
+
+
+				found = true;
+        	}
+        	else
+        	{
+        		// Only detect if the face is between a margin
+		        if(distEuclidean(pnt.x, prevPoint.x, pnt.y, prevPoint.y) <= TOTAL_VARIATION)
+		        {
+		        	// Correct the number of Frames
+		        	numFrames = 0;
+
+		        	// Update the measure
+		        	measure.at<float>(0) = pnt.x;
+					measure.at<float>(1) = pnt.y;
+		        	
+		        	// Update the previus point
+		        	prevPoint = pnt;
+
+		        	// Correct the position
+					KF.correct(measure);
+		        }
+		        else
+		        {
+		        	// The detection is outside the range
+		        	if(numFrames >= 5)
+		        	{
+		        		prevPoint = cv::Point(320,240);
+	    				found = false;
+		        	}
+		        	else
+		        	{
+		        		numFrames++;
+		        	}
+		        	//prevPoint = kalmanCenter;
+		        }
+        	}
+
+	    }
+	    else
+	    {
+	    	// The detection is lost more than 10 frames
+	    	if( numFrames >= 5) 
+	    	{
+	    		//cout << "Lo he perdido" << numFrames << std::endl;   		
+	    		prevPoint = cv::Point(320,240);
+	    		found = false;
+	    	}
+	    	else
+	    	{
+	    		numFrames++;
+	    	}
+	    }
+
+
+		imshow( "test2", frame);
+
+		// Return value
+		if(found && numFrames == 0)
+		{
+			// Face detected
+			p.x = prevPoint.x;
+			p.y = prevPoint.y;
+		}
+		else if(found)
+		{
+			// Return Kalman values
+			p.x = static_cast<int>(kalmanCenter.x);
+			p.y = static_cast<int>(kalmanCenter.y);
+		}
+		else
+		{
+			// return a dummy value
+			p.x = 1000;
+			p.y = 1000;
+		}
+
+	    point_pub.publish(p);
+
+		ros::spinOnce();
+		loop_rate.sleep();
+
+		// Se actualiza la variable de tiempo
+		preTick = ticks;
+
+		// If the 'q' or 'Esc' is pressed, exit the loop
+        int c = waitKey(10);
+        if( (char)c == 27 || (char)c == 'q') 
+        { 
+        	break; 
+        } // escape
+    } 
+	return 0;
+}
+
+/** @function detectFacePosition */
+// This function returns 1000,1000 if there is no face
+// else, returns the nearest face detected from the last detection
+Point detectFace(Mat frame, Point lastPoint)
+{
+	vector<Rect> faces;
+    Mat frame_gray;
+    Point nearest_face(1000,1000);
+
+    int min_dist=1000;
+
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    equalizeHist( frame_gray, frame_gray );
+
+    //-- Detect faces
+    face_cascade.detectMultiScale(	frame_gray, // Image
+									faces, 		// Faces location
+									1.3, 		// Scale factor
+									4,			// min Neighbors
+									0|CV_HAAR_DO_CANNY_PRUNING,			// Flags
+									Size(30,30));	// Min Size
+    
+    if(faces.size()==1){
+    		rectangle(frame, faces[0], cv::Scalar(255,0,0), 2);
+			nearest_face.x = faces[0].x + faces[0].width*0.5;
+			nearest_face.y = faces[0].y + faces[0].height*0.5;
+    }else if(faces.size()>1){
+    	for( size_t i = 0; i < faces.size(); i++ ){
+    		rectangle(frame, faces[i], cv::Scalar(255,0,0), 2);
+    		int distEucl = distEuclidean(	faces[i].x + faces[i].width*0.5,
+    										lastPoint.x,
+    										faces[i].y + faces[i].height*0.5,
+    										lastPoint.y);
+    		if(distEucl < min_dist){
+    			nearest_face.x = faces[i].x;
+    			nearest_face.y = faces[i].y;
+    			min_dist = distEucl;
+    		}
+    	}
+    }
+
+    //imshow( "test", frame);
+    return nearest_face;
+}
+
+int distEuclidean(int x1,int x2, int y1, int y2)
+{
+	return sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+}
+
+
+
---
+++ lookat/lookat_control/src/main.cpp
@@ -0,0 +1,315 @@
+/*
+*
+*   LookAt control.- 
+*       This program get the value from the detection module, 
+*       and send it to arduino to move the servos
+*       Authors: Sandra & David
+*/
+
+// Librerias para incluir
+#include <iostream>
+#include <vector>
+
+// Includes to PID
+#include <ctime>
+
+// ROS includes
+#include "ros/ros.h"
+
+#include "geometry_msgs/Point.h"
+
+#include "std_msgs/MultiArrayLayout.h"
+#include "std_msgs/MultiArrayDimension.h"
+#include "std_msgs/Int32MultiArray.h"
+
+// Valores para ajustar los valores de los grados
+const float DEGREE_ADJUST_X = 0.14f;
+const float DEGREE_ADJUST_Y = 0.1875f;
+
+// Valores para definir el centro de la imagen
+const int WIDTH = 640;
+const int HEIGHT = 480;
+
+// Prototipo de las funciones
+void pointCallback(const geometry_msgs::Point::ConstPtr& point);
+
+// Flag variable to control a correct data
+bool dataReceived = false;
+
+// Se inicializan las variables para detectar la posición el objeto
+int x = 0; 
+int y = 0;
+
+// Variable de control de la detección de cara
+bool detected = false;
+
+// Definición de los margenes máximos y mínimos del Tilt
+const int MAX_TILT = 30;
+const int MIN_TILT = 100;
+
+// Bloque principal
+int main(int argc, char **argv)
+{
+    // Ros Init
+    ros::init(argc, argv, "LookAt_Control");
+    ros::NodeHandle n;
+
+    // Publisher to the Arduino board
+    ros::Publisher Servo_pub = n.advertise<std_msgs::Int32MultiArray>("PAT", 1000);
+    ros::Rate loop_rate(25);
+
+    // Subscriber to the detectface
+    ros::Subscriber sub = n.subscribe("pointpub", 1000, pointCallback);
+
+    // Variables para el centro del sistema de control
+    // Se inicializa con los valores medios de la pantalla
+    float xControl = WIDTH / 2.0f;
+    float yControl = HEIGHT / 2.0f;
+
+    // Variables de control que serán las mismas para x e y.
+    float kProp = 0.1f; // Ganancia proporcional
+    float kInt = 0.01f; // Ganancia integral
+    float kDeriv = 0.001f; // Ganancia derivativa
+
+    // variables para los errores para X
+    float errorAct_X = 0.0f;
+    float errorAnt_X = 0.0f;
+    float errorAcum_X = 0.0f; // Variable de error acumulado para cuando ha detectado
+    float errorAcum_no_X = 0.0f; // Variable de error acumulado para cuando no ha detectado
+
+    // variables para los errores para Y
+    float errorAct_Y = 0.0f;
+    float errorAnt_Y = 0.0f;
+    float errorAcum_Y = 0.0f;    // Variable de error acumulado para cuando ha detectado
+    float errorAcum_no_Y = 0.0f; // Variable de error acumulado para cuando no ha detectado
+
+    // Salida del controlador en X
+    float outputController_X = 0.0f;
+
+    // Salida del controlador en Y
+    float outputController_Y = 0.0f;
+
+    // Variable para guardar el periodo, que variará dependiendo de lo que tarde en ejecutar el código
+    //float samplePeriod = 0.0f;
+
+    // Variables para el cálculo del tiempo
+    //clock_t reloj;
+    ros::Time current_time, last_time;
+
+    // Init time
+    current_time = ros::Time::now();
+    last_time = current_time;
+
+    // Variable de control de los grados de giro o yaw
+    int degrees_X = 0; // se inicializa como 0
+
+    // Variable de control de los grados de inclinación o pitch
+    int degrees_Y = 0; // se inicializa en el 0
+
+
+
+    while(ros::ok())
+    {
+        if(dataReceived)
+        {
+            // Se captura el tiempo de inicio del bucle
+            current_time   = ros::Time::now();
+
+            // Se actualiza el valor del periodo de muestreo
+            double samplePeriod = (current_time - last_time).toSec();
+            //std::cout << "Sample time: " << samplePeriod << std::endl;
+
+            dataReceived = false;   
+            // Sistema de control 
+            if (detected)
+            {
+                // Si hay un objeto en pantalla, entonces se mueve el puntero hacia el
+                
+                // Se ejecuta el control para X
+
+                // Se ajusta el error actual
+                errorAct_X = x - (WIDTH/2);
+
+                // Se actualiza el error integral
+                errorAcum_X += errorAct_X;
+
+                // Se calcula la salida del controlador
+                // u = Kp * error + Kint * Ts * errorAcumulado + Kder * (e - e(t-1))/Tsample
+                outputController_X = kProp * errorAct_X + kInt * samplePeriod * errorAcum_X + (kDeriv * (errorAct_X - errorAnt_X)) / samplePeriod;
+
+                // Se actualiza el valor de la posición 
+                // Xsalida = Xactual + corrección en x
+                xControl += outputController_X;
+
+                // Se guarda el valor de error para el control derivativo
+                errorAnt_X = errorAct_X;
+
+                // Se ejecuta el control para Y
+
+                // Se ajusta el error actual
+                errorAct_Y = y - (HEIGHT/2);
+
+                // Se actualiza el error integral
+                errorAcum_Y += errorAct_Y;
+
+                // Se calcula la salida del controlador
+                // u = Kp * error + Kint * Ts * errorAcumulado + Kder * (e - e(t-1))/Tsample
+                outputController_Y = kProp * errorAct_Y;// + kInt * samplePeriod * errorAcum_Y + (kDeriv * (errorAct_Y - errorAnt_Y)) / samplePeriod;
+
+                // Se actualiza el valor de la posición 
+                // Ysalida = Yactual + corrección en y
+                yControl += outputController_Y;
+
+
+                // Se guarda el valor de error para el control derivativo
+                errorAnt_Y = errorAct_Y;
+            }
+            else
+            {
+                // TODO: Decidir que se hará en reposo.
+                // Si no hay un objeto en pantalla, entonces se vuelve al centro.
+                
+                // Se ejecuta el control para X
+
+                // Se ajusta el error actual
+                errorAct_X = (WIDTH / 2.0f) - xControl;
+
+                // Se actualiza el error integral
+                errorAcum_X += errorAct_X;
+
+                // Se calcula la salida del controlador
+                // u = Kp * error + Kint * Ts * errorAcumulado + Kder * (e - e(t-1))/Tsample
+                outputController_X = kProp * errorAct_X + kInt * samplePeriod * errorAcum_X + (kDeriv * (errorAct_X - errorAnt_X)) / samplePeriod;
+
+                // Se actualiza el valor de la posición 
+                // Xsalida = Xactual + corrección en x
+                xControl += outputController_X;
+
+                // Se guarda el valor de error para el control derivativo
+                errorAnt_X = errorAct_X;
+
+                // Se ejecuta el control para Y
+
+                // Se ajusta el error actual
+                errorAct_Y = (HEIGHT / 2.0f) - yControl;
+
+                // Se actualiza el error integral
+                errorAcum_Y += errorAct_Y;
+
+                // Se calcula la salida del controlador
+                // u = Kp * error + Kint * Ts * errorAcumulado + Kder * (e - e(t-1))/Tsample
+                outputController_Y = kProp * errorAct_Y + kInt * samplePeriod * errorAcum_Y + (kDeriv * (errorAct_Y - errorAnt_Y)) / samplePeriod;
+
+                // Se actualiza el valor de la posición 
+                // Ysalida = Yactual + corrección en y
+                yControl += outputController_Y;
+
+                // Se guarda el valor de error para el control derivativo
+                errorAnt_Y = errorAct_Y;
+            }
+
+            // Se ajusta el error en grados, tanto en X como en Y
+            // Esta parte del código será la que mueva el motor
+
+            // Se adapta la salida en X a grados, y se redondea a int
+            // El punto de reposo en X será a 90 grados.
+            degrees_X = 90 - static_cast<int>((xControl - 320) * DEGREE_ADJUST_X);
+
+            // Se actualiza el valor para que no pase de 0 o 180
+            if(degrees_X > 180)
+            {
+                // Se fuerza el valor máximo
+                degrees_X = 180;
+            }
+
+            if(degrees_X < 0)
+            {
+                // Se fuerza el valor mínimo
+                degrees_X = 0;
+            }
+           
+
+            // Se adapta la salida en Y a grados, y se redondea a int
+            // El punto de reposo en y será a 115 grados
+            degrees_Y = 65 + static_cast<int>((yControl - 240) * DEGREE_ADJUST_Y);
+
+            // Se actualiza el valor para que no pase de los valores definidos
+            // en los margenes
+            if(degrees_Y < MAX_TILT)
+            {
+                // Se fuerza el valor máximo
+                degrees_Y = MAX_TILT;
+            }
+
+            if(degrees_Y > MIN_TILT)
+            {
+                // Se fuerza el valor mínimo
+                degrees_Y = MIN_TILT;
+            }
+
+
+            // Se imprime el resultado
+            /*std::cout << "Degrees X: " << degrees_X << " Degrees_Y: " << degrees_Y << std::endl;
+            std::cout << "X: " << x << " y el error " << errorAct_X;
+            std::cout << " Y: " << y <<  " y el error" << errorAct_Y << std::endl;*/
+
+            // Se envía el valor calculado al Arduino para que actualize la posición de inclinación
+            
+
+            // Define the array msg variable
+            std_msgs::Int32MultiArray array;
+
+            // Clear the data
+            array.data.clear();
+
+            array.data.push_back(degrees_X);
+            /*if(yaw < 180)
+            {
+                array.data.push_back(yaw);
+                yaw++;
+            }
+            else
+            {
+                yaw = 0;
+            }*/
+            array.data.push_back(degrees_Y);
+
+            Servo_pub.publish(array);
+
+            // Lo printo en la consola, en vez de en pantalla
+            //std::cout << "El angulo a corregir en Y: " << degrees_Y << std::endl;
+        }
+
+        // Update the timer
+        last_time = current_time;
+
+        // Ros
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    // Se sale del programa
+	return 0;
+}
+
+// ROS subscriber function
+void pointCallback(const geometry_msgs::Point::ConstPtr& point)
+{
+    geometry_msgs::Point new_point = *point;
+    x = static_cast<int>(new_point.x);
+    y = static_cast<int>(new_point.y);
+    if(x >= 1000 && y >= 1000)
+    {
+        // There is no face detected
+        detected = false;
+    }
+    else
+    {
+        detected = true;
+    }
+
+    // Prueba de envio de comandos
+    //std::cout << "El valor de x es : " << x << " y de y: " << y << std::endl;
+    // Habilitate the control
+    dataReceived = true;   
+}
